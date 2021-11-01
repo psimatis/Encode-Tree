5a83d177641cc53689f89ebd5cb401b523d4bab7
psimatis <psimatis@connect.ust.hk>
2021-11-01 17:09:27 +0800

    changed loss function

--- main.cpp
+++ main.cpp
@@ -13,63 +13,55 @@ int main() {
     cout << (cuda_available ? "CUDA available. Training on GPU." : "Training on CPU.") << '\n';
 
     // Hyper parameters
-    const int64_t h_dim = 400;
-    const int64_t z_dim = 20;
-    const int64_t input_size = 4;
-    const int64_t batch_size = 100;
-    const size_t num_epochs = 3;
+    const int64_t hSize = 2;
+    const int64_t codeSize = 1;
+    const int64_t inputSize = 4;
+    const int64_t batchSize = 1024;
+    const size_t epochs = 3;
     const double learning_rate = 1e-3;
 
 	// Data
-    // auto dataset = torch::data::datasets::MNIST(MNIST_data_path).map(torch::data::transforms::Stack<>());
     auto dataset = CustomDataset("../data/4D-1e6_norm.csv").map(torch::data::transforms::Stack<>());
 	auto num_samples = dataset.size().value();
-	auto dataloader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(move(dataset), batch_size);
+	auto dataloader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(move(dataset), batchSize);
 
     // Model
-    VAE model(input_size, h_dim, z_dim);
+    VAE model(inputSize, hSize, codeSize);
     model->to(device);
-
-    // Optimizer
     torch::optim::Adam optimizer(model->parameters(), torch::optim::AdamOptions(learning_rate));
 
     // Set floating point output precision
     cout << fixed << setprecision(4);
 
-    cout << "Training...\n";
-
     // Train the model
-    for (size_t epoch = 0; epoch != num_epochs; ++epoch) {
-        torch::Tensor images;
+    for (size_t epoch = 0; epoch != epochs; ++epoch) {
+        torch::Tensor inputs;
         size_t batch_index = 0;
-
         model->train();
 
         for (auto& batch : *dataloader) {
             // Transfer images to device
-            images = batch.data.reshape({-1, input_size}).to(device);
+            inputs = batch.data.reshape({-1, inputSize}).to(device);
 
             // Forward pass
-            auto output = model->forward(images);
-	        auto reconstruction_loss = torch::nn::functional::binary_cross_entropy(output.reconstruction, images,
-                torch::nn::functional::BinaryCrossEntropyFuncOptions().reduction(torch::kSum));
+            auto output = model->forward(inputs);
+			auto loss = torch::nn::functional::mse_loss(output.reconstruction, inputs);
 
             // Backward pass and optimize
-            auto loss = reconstruction_loss;
             optimizer.zero_grad();
             loss.backward();
             optimizer.step();
 
             if ((batch_index + 1) % 100 == 0) {
-                cout << "Epoch [" << epoch + 1 << "/" << num_epochs << "], Step [" << batch_index + 1 << "/"
-                    << num_samples / batch_size << "], Reconstruction loss: "
-                    << reconstruction_loss.item<double>() / batch.data.size(0) << "\n";
+                cout << "Epoch [" << epoch + 1 << "/" << epochs 
+                	<< "], Step [" << batch_index + 1 << "/" << num_samples / batchSize 
+                	<< "], Loss: " << loss.item<double>() / batch.data.size(0) << "\n";
             }
-            ++batch_index;
+            batch_index++;
         }
 
         model->eval();
-        torch::NoGradGuard no_grad;\
+        torch::NoGradGuard no_grad;
     }
 	cout << "Training finished!\n";
 }
